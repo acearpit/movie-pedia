e153dc72bf671ea6605c79d941e1cd8966ef5a64
Arpit Gupta <agsinghgarg131@gmail.com>
2021-09-08 22:33:16 +0530

    Fixed the styling of navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,24 @@
 import "./Navbar.css";
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 const navbar = (props) => (
-  <div className="Navbar">
-    <a href="/movie-pedia">
-      <img src="https://pngimage.net/wp-content/uploads/2018/06/mp-png-5.png" className="img" />
-    </a>
-    <div className="searchbar">
+  <div className="navbar">
+    <Link to="/movie-pedia" className="nav_left">
+      <img src="https://pngimage.net/wp-content/uploads/2018/06/mp-png-5.png" />
+    </Link>
+    <div className="nav_center">
       <input type="text" placeholder="Search here..." className="input" id="searchBar" onChange={props.handleSearch} />
     </div>
-    <button type="submit" className="login">
-      Login
-    </button>
-    <button type="submit" className="signup">
-      Signup
-    </button>
+    <div className="nav_right">
+      <button type="submit" className="login">
+        Login
+      </button>
+      <button type="submit" className="signup">
+        Signup
+      </button>
+    </div>
   </div>
 );
 
